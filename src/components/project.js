7c587380ea5118656b905e61d1e35960499eee6f
Sam Low-Chappell <sam@hackeryou.com>
2019-10-10 17:11:16 -0400

    improved project page responsive

--- src/components/project.js
+++ src/components/project.js
@@ -3,51 +3,26 @@ import projectStyle from './project.module.css';
 
 const Project = ({ title, subtitle, description, image, links, index }) => {
     return (
-        <>
-            {index % 2 === 0 ? (
-                <div className={projectStyle.project}>
-                    <div className={projectStyle.projectImage}>
-                        <a href={links.live}>
-                            <img src={image} alt="" />
-                        </a>
-                    </div>
-                    <div>
-                        <h2>{title}</h2>
-                        <p>{subtitle}</p>
-                        <p>{description}</p>
-                        <div className={projectStyle.projectLinks}>
-                            <a href={links.live}>
-                                <button>View it live</button>
-                            </a>
-                            <a href={links.gitHub}>
-                                <button>View the code</button>
-                            </a>
-                        </div>
-                    </div>
+        <div className={projectStyle.project}>
+            <div className={projectStyle.projectImage}>
+                <a href={links.live}>
+                    <img src={image} alt="" />
+                </a>
+            </div>
+            <div>
+                <h2>{title}</h2>
+                <p>{subtitle}</p>
+                <p>{description}</p>
+                <div className={projectStyle.projectLinks}>
+                    <a href={links.live}>
+                        <button>View it live</button>
+                    </a>
+                    <a href={links.gitHub}>
+                        <button>View the code</button>
+                    </a>
                 </div>
-            ) : (
-                <div className={projectStyle.project}>
-                    <div>
-                        <h2>{title}</h2>
-                        <p>{subtitle}</p>
-                        <p>{description}</p>
-                        <div className={projectStyle.projectLinks}>
-                            <a href={links.live}>
-                                <button>View it live</button>
-                            </a>
-                            <a href={links.gitHub}>
-                                <button>View the code</button>
-                            </a>
-                        </div>
-                    </div>
-                    <div className={projectStyle.projectImage}>
-                        <a href={links.live}>
-                            <img src={image} alt="" />
-                        </a>
-                    </div>
-                </div>
-            )}
-        </>
+            </div>
+        </div>
     );
 };
 
