648572b19ad5ab6a5453914092f206cd5d988c51
Sam Low-Chappell <sam@hackeryou.com>
2019-10-10 12:28:57 -0400

    Updated fonts, added normalize

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-google-fonts`,
             options: {
-                fonts: [
-                    `limelight`,
-                    `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-                ],
+                fonts: [`Cormorant Garamond`, `Roboto`, `Montserrat`],
                 display: 'swap',
             },
         },
