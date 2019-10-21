066d50c71cc87b3bcb89a63b2084551d729d026c
Sam Low-Chappell <sam@hackeryou.com>
2019-10-21 10:05:13 -0400

    added more styling for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,38 +7,53 @@ const Contact = () => {
     return (
         <Layout pageTitle="drop me a line">
             <SEO title="Contact" />
-            <form action="submit">
-                <div className={contactStyles.nameAndEmailContainer}>
-                    <div className={contactStyles.inputContainer}>
-                        <label for="name">Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            placeholder="Obi Wan Kenobi"
-                        />
-                    </div>
-                    <div className={contactStyles.inputContainer}>
-                        <label for="email">Email</label>
-                        <input
-                            type="text"
-                            id="email"
-                            placeholder="obi.kenobi@dagoba.net"
-                        />
-                    </div>
+            <div className={contactStyles.contactContainer}>
+                <div>
+                    <h2>I'd love to work together.</h2>
+                    <p>
+                        Have a freelance project? Looking to hire a hungry
+                        junior? Want to chat southern Ontario climbing routes??
+                        I'm your guy.
+                    </p>
+                    <p>
+                        Send me an email at sjlowchappell at gmail.com or fill
+                        out this handy form. I'm looking forward to hearing from
+                        you.
+                    </p>
                 </div>
+                <form action="submit">
+                    <div className={contactStyles.nameAndEmailContainer}>
+                        <div className={contactStyles.inputContainer}>
+                            <label for="name">Name</label>
+                            <input
+                                type="text"
+                                id="name"
+                                placeholder="Obi Wan Kenobi"
+                            />
+                        </div>
+                        <div className={contactStyles.inputContainer}>
+                            <label for="email">Email</label>
+                            <input
+                                type="text"
+                                id="email"
+                                placeholder="obi.kenobi@dagoba.net"
+                            />
+                        </div>
+                    </div>
 
-                <div className={contactStyles.inputContainer}>
-                    <label for="message">Message</label>
-                    <textarea
-                        name="message"
-                        id="message"
-                        rows="10"
-                        placeholder="He's more machine than man now..."
-                    ></textarea>
-                </div>
+                    <div className={contactStyles.inputContainer}>
+                        <label for="message">Message</label>
+                        <textarea
+                            name="message"
+                            id="message"
+                            rows="10"
+                            placeholder="He's more machine than man now..."
+                        ></textarea>
+                    </div>
 
-                <button>Submit</button>
-            </form>
+                    <button>Submit</button>
+                </form>
+            </div>
         </Layout>
     );
 };
