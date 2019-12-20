27a33af88f13c122768bb621ef0c3af537adb917
Sam Low-Chappell <sam@hackeryou.com>
2019-12-20 11:32:17 -0500

    moved project data to utils file

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,69 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Project from '../components/project';
-import fermentedFilms from '../assets/fermentedFilmsMockup.png';
-import podcastable from '../assets/podcastableMockup.png';
-import songSmash from '../assets/songSmashMockup.png';
-import gourmand from '../assets/foodPassionMockup.png';
-import vacay from '../assets/vacayMockup.png';
-
-const projectData = [
-    {
-        image: fermentedFilms,
-        title: 'Fermented Films',
-        subtitle: 'JavaScript | React | API',
-        description:
-            'Fermented Films is a one stop shop for a curated evening based on one ingredient. Built using the React library, this project utilizes the MovieDB, CocktailDB, and MealsDB APIs.',
-        links: {
-            live: 'https://sharp-goldberg-70cf0b.netlify.com/',
-            gitHub: 'https://github.com/sjlowchappell/fermented-films',
-        },
-    },
-    {
-        image: vacay,
-        title: 'Vacay Tracker',
-        subtitle: 'JavaScript | React | Firebase',
-        description:
-            'Vacay Tracker allows you to budget and track your expenses while traveling. Built with React and Firebase, this is the best tool to make sure your vacation spending is under control.',
-        links: {
-            live: 'https://wonderful-keller-24755d.netlify.com/',
-            gitHub: 'https://github.com/sjlowchappell/vacation-tracker',
-        },
-    },
-    {
-        image: podcastable,
-        title: 'Podcastable',
-        subtitle: 'JavaScript | React | API ',
-        description:
-            'Podcastable is your home for podcast recommendations. Built with React and leveraging the ListenNotes API. Search your favorite podcast and get a list of up to 8 recommendations based on the searched value.',
-        links: {
-            live: 'https://priceless-ardinghelli-25423e.netlify.com/',
-            gitHub: 'https://github.com/sjlowchappell/podcastable',
-        },
-    },
-    {
-        image: songSmash,
-        title: 'Song Smash',
-        subtitle: 'JavaScript | API | SCSS',
-        description:
-            'Song Smash is a project using modern vanilla JavaScript. Enter your favorite song and get its lyrics. Smash the song, and see a silly result. Utilizes the Merriam Webster and Musixmatch APIs.',
-        links: {
-            live: 'https://infallible-villani-1384e8.netlify.com/',
-            gitHub: 'https://github.com/sjlowchappell/songSmasher',
-        },
-    },
-    {
-        image: gourmand,
-        title: 'Le Gourmand',
-        subtitle: 'HTML | SCSS',
-        description:
-            'Le Gourmand is a simple design conversion using HTML and SCSS. Built using modern techniques and with accessibility in mind.',
-        links: {
-            live: 'https://fervent-williams-393db6.netlify.com/',
-            gitHub: 'https://github.com/sjlowchappell/gourmetRestaurant',
-        },
-    },
-];
+import projectData from '../utils/projectData';
 
 const Projects = () => (
     <Layout pageTitle="projects">
