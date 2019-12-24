03b4b38f2937dde67da9ea70df972d8350c7c2b3
Sam Low-Chappell <sam@hackeryou.com>
2019-12-24 07:47:54 -0500

    added hamburger menu functionality on responsive

--- src/components/header.js
+++ src/components/header.js
@@ -4,30 +4,40 @@ import React from 'react';
 import github from '../assets/github-1.svg';
 import instagram from '../assets/instagram-glyph-1.svg';
 import linkedin from '../assets/linkedin-icon-1.svg';
-import './header.module.css';
+import styles from './header.module.css';
 import resume from '../assets/resume.pdf';
 
 const Header = ({ pageTitle }) => (
     <header>
-        <nav>
+        <input
+            type="checkbox"
+            id="hamburger"
+            className={styles.hamburgerInput}
+        />
+        <div className={styles.hamContainer}>
+            <span className={styles.hamburgerLine}></span>
+            <span className={styles.hamburgerLine}></span>
+            <span className={styles.hamburgerLine}></span>
+        </div>
+        <nav className={styles.nav}>
             <ul>
                 <li>
                     <a href="https://github.com/sjlowchappell">
-                        <img src={github} alt="" className="svg-filter" />
+                        <img src={github} alt="GitHub Icon" className={styles.icon} />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.instagram.com/sammylowchappy/">
-                        <img src={instagram} alt="" />
+                        <img src={instagram} alt="Instagram Icon" className={styles.icon} />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/samuel-low-chappell-626110b3/">
-                        <img src={linkedin} alt="" />
+                        <img src={linkedin} alt="Linkedin Icon" className={styles.icon} />
                     </a>
                 </li>
             </ul>
-            <ul>
+            <ul className={styles.subnav}>
                 <li>
                     <Link to="/">About</Link>
                 </li>
